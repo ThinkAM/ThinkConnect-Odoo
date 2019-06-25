14c9faeba3ed7090dc0eae88f9c6f3701d1014b8
Felipe A. de Almeida <felipe.almeida@thinkam.net>
2019-06-25 02:17:21 -0300

    Ajustada a versão e o nome

--- think_connect/__manifest__.py
+++ think_connect/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Think Connect',
-    'version': '12.0.1.1',
+    'version': '12.0.1.2',
     'category': 'Think A.M.',
     'description': u"""
 This module has been created by Think A.M..
